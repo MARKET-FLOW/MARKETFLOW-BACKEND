0e41cd8f5a4041972b0201f681921fdc99e22945
Barry Ali <pavelnemesis288@gmail.com>
2026-04-22 01:53:39 +0000

    feat/config: Mise en place swagger + redoc et resource users

---
+++ app/src/common/types/crud.result.ts
@@ -0,0 +1,39 @@
+import { GlobalAppResult } from './global-app.result';
+
+export class CRUDResult<T> extends GlobalAppResult<T> {
+  public readonly statusCode: number;
+
+  constructor(
+    data: T | null = null,
+    error: string | null = null,
+    statusCode: number = 200,
+  ) {
+    super(data, error);
+    this.statusCode = statusCode;
+  }
+
+  /**
+   * Helper pour créer une réponse de succès.
+   */
+  static crud_success<T>(data: T, statusCode: number = 200): CRUDResult<T> {
+    return new CRUDResult<T>(data, null, statusCode);
+  }
+
+  /**
+   * Helper pour créer une réponse d'erreur.
+   */
+  static crud_error<T>(
+    message: string,
+    statusCode: number = 500,
+  ): CRUDResult<T> {
+    return new CRUDResult<T>(null, message, statusCode);
+  }
+
+  // Équivalent du __repr__ pour le debug
+  toString(): string {
+    if (this.isSuccess) {
+      return `[CRUDResult Success] Status: ${this.statusCode}, Data: ${JSON.stringify(this.data)}`;
+    }
+    return `[CRUDResult Error] Status: ${this.statusCode}, Error: ${this.error}`;
+  }
+}
--- app/src/main.ts
+++ app/src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import redoc from 'redoc-express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('MARKET-FLOW API Documentation')
+    .setDescription(
+      'Notre application backend de gestion et suivi des ventes de produits',
+    )
+    .setVersion('1.0')
+    .addTag('nestjs')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  // redoc configuration
+  app.use(
+    'redocs',
+    redoc({
+      title: 'MARKET-FLOW API Documentation',
+      specUrl: '/docs-json',
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
