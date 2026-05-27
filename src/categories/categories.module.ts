8babf23ca8fc47e12c3f094c0d7a24da7aa5946d
Emmanuel-07-April <emmanuelamouzou005@gmail.com>
2026-05-27 11:52:04 +0000

    feat: ajout cache Redis, Mapper et FrontReadCategory dans le module catégories

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,10 +1,16 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
 import { CategoriesRepository } from './categories.repository';
+import { RedisCacheService } from 'src/common/cache/redis-cache.service';
 
 @Module({
   controllers: [CategoriesController],
-  providers: [CategoriesService, CategoriesRepository],
+  providers: [
+    CategoriesService,
+    CategoriesRepository,
+    RedisCacheService,
+  ],
 })
 export class CategoriesModule {}
